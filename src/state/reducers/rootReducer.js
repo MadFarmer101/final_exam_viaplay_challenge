3446bcbb5cdf25f8a44e97c1d1ee046d8028335d
Janko <mailodjanka@gmail.com>
2020-04-22 17:09:32 +0200

    Makes App.jsx a functional component + adds basic redux data structure

---
+++ src/state/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import initialState from "../store/initialState";
+import * as actionTypes from "../actions/actionTypes";
+
+const rootReducer = (state = initialState, actions) => {};
+
+export default rootReducer;
