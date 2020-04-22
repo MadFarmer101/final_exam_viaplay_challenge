3fed1c8f47d0cf9c5ff735030e92a8b4cdf5cb61
Janko <mailodjanka@gmail.com>
2020-04-22 17:19:09 +0200

    Adds get series index data action in root reducer

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,6 +1,14 @@
 import initialState from "../store/initialState";
 import * as actionTypes from "../actions/actionTypes";
 
-const rootReducer = (state = initialState, actions) => {};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.GET_SERIES_INDEX_DATA:
+      return {
+        ...state,
+        ...action.payload,
+      };
+  }
+};
 
 export default rootReducer;
